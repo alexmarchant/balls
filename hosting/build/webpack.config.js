49aa0b21d674ab722328ac7c5143ffc9802afdc2
Alex Marchant <alexjmarchant@gmail.com>
2017-11-28 21:43:04 -0500

    Fix some build settings

--- hosting/build/webpack.config.js
+++ hosting/build/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: path.resolve(__dirname, '..', 'js', 'bundle.js'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '..', '..', 'public'),
+    path: path.resolve(__dirname, '..', 'public'),
   },
   module: {
     rules: [
